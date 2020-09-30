c5c2d51a61bcea500c285093294de060db06ff9f
Mohammad Talha <talhaviet22@gmail.com>
2020-09-30 15:15:54 +0530

    appbar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,6 +9,28 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        elevation: 6,
+        title: Text(
+          "Todo App",
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15.0),
+            child: IconButton(
+              icon: Icon(
+                Icons.refresh,
+                semanticLabel: "Refresh",
+                size: 38,
+              ),
+              onPressed: () {
+                print("refresh");
+              },
+            ),
+          ),
+        ],
+      ),
       body: Container(
         child: Center(
           child: Text("WELCOME"),

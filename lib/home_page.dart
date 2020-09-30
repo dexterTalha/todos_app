4b6e2b2eef3a2359637fa8aa1393aaed517896b6
Mohammad Talha <talhaviet22@gmail.com>
2020-09-30 16:10:03 +0530

    drawer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,6 +9,37 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              color: Colors.green,
+              child: SafeArea(
+                  child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0, right: 10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(50)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: FlutterLogo(
+                          size: 50,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text("User name"),
+                ],
+              )),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Colors.green,
         elevation: 6,
